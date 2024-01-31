faea345863469c00f89542b418340fde830b37cc
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-31 22:41:06 +0545

    feat: Added some logcal code for data types

---
+++ basic/data_type.py
@@ -0,0 +1,31 @@
+# a="Hemlal"
+# print(type(a))
+# print(type(a)==str)
+# print(isinstance(a,str))
+
+#* Constructor function
+# pizza=str("Dulal")
+# print(type(pizza))
+# print(type(pizza)==str)
+# print(isinstance(pizza,str))
+
+#* Concatenation 
+# first="Hemlal"
+# last="Dulal"
+# print(first + " "+ last)
+
+#* Casting a number to a string
+# decade=str(1999)
+# print(type(decade))
+
+#* Multiline sentences
+multiline=''' Hey how are you?
+          I'm All good'''
+print(multiline)
+print(len(multiline))
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+#* Build a menu
+print("Coffiee".ljust(16,".") + "$1".rjust(4))
