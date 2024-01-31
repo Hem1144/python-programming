15a725b7b535db3209c490650f338f74afd48bb7
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-01-31 22:54:12 +0545

    feat: Checked different data types and usecases

--- basic/data_type.py
+++ basic/data_type.py
@@ -1,31 +1,55 @@
+myName = "Hemlal Dulal"
+
 # a="Hemlal"
 # print(type(a))
 # print(type(a)==str)
 # print(isinstance(a,str))
 
-#* Constructor function
+# * Constructor function
 # pizza=str("Dulal")
 # print(type(pizza))
 # print(type(pizza)==str)
 # print(isinstance(pizza,str))
 
-#* Concatenation 
+# * Concatenation
 # first="Hemlal"
 # last="Dulal"
 # print(first + " "+ last)
 
-#* Casting a number to a string
+# * Casting a number to a string
 # decade=str(1999)
 # print(type(decade))
 
-#* Multiline sentences
-multiline=''' Hey how are you?
-          I'm All good'''
-print(multiline)
-print(len(multiline))
-print(len(multiline.strip()))
-print(len(multiline.lstrip()))
-print(len(multiline.rstrip()))
+# * Multiline sentences
+# multiline=''' Hey how are you?
+#           I'm All good'''
+# print(multiline)
+# print(len(multiline))
+# print(len(multiline.strip()))
+# print(len(multiline.lstrip()))
+# print(len(multiline.rstrip()))
 
-#* Build a menu
-print("Coffiee".ljust(16,".") + "$1".rjust(4))
+# * Build a menu
+# print("Coffiee".ljust(16,".") + "$1".rjust(4))
+
+
+#! string index value
+# myName="Hemlal Dulal"
+# print(myName[1])
+# print(myName[-1])
+# print(myName[1:-1])
+# print(myName[1:])  #* desire index to last index
+
+#! method ruturn boolean data
+# print(myName.startswith("D"))
+# print(myName.endswith("l"))
+
+#! Numeric data type
+# price = 100
+# best_price = int(80)
+# print(type(price))
+# print(isinstance(best_price, int))
+
+#! complex data type
+comp_val = 5 + 10j
+print(type(comp_val))
