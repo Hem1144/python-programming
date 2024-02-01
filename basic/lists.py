375cad6186818e8111f77ada621f041afd891865
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-01 13:46:35 +0545

    feat: Added almost methods of list and also implement the tuple

---
+++ basic/lists.py
@@ -0,0 +1,89 @@
+users = [
+    "Hemlal",
+    "Prasad",
+    "Dulal",
+]
+emptyList = []
+
+data = ["Hemlal", 25, True]
+
+nums = [4, 23, 54, 21, 8]
+# print("Hemlal" in emptyList)
+
+# print(users[0])
+# print(users[-1])
+# print(users[-2])
+
+# print(users.index("Dulal"))
+
+# print(users[0:2])
+# print(users[1:])
+# print(len(data))
+
+# users.append("Sara")
+# print(users)
+
+# users += ["Johnson"]
+# print(users)
+
+# users.extend(["Somina", "Roman"])
+# print(users)
+
+#! delete data
+# del data
+# print(data)
+# data.clear()
+# print(data)
+
+#! sort
+# users.sort()
+# print(users)
+
+# users[1:2] = ["dev"]
+# users.sort(key=str.lower)
+# print(users)
+
+#! for numbers
+# nums.reverse()
+# print(nums)
+
+# nums.sort(reverse=True)  # * it sort the array in decending order
+# print(nums)
+
+# print(sorted(nums, reverse=True))
+# print(nums)
+
+#! ways to copy of list
+# numsCopy = nums.copy()
+# mynums = list(nums)
+# myCopy = nums[:]
+
+# print(numsCopy)
+# print(mynums)
+# myCopy.sort()
+# print(myCopy)
+# print(nums)
+
+# print(type(nums))
+
+# myList = list([1, "Hemlal", True])
+# print(myList)
+
+#! Tuples
+# myTuple = tuple(("Hemlal", 25, True))
+# anotherTuple = tuple((1, 4, 6, 8, 6, 6))
+
+# print(myTuple)
+# print(type(myTuple))
+
+# newList = list(myTuple)
+# newList.append("Prasad")
+# newTuple = tuple(newList)
+# print(newTuple)
+
+# (one, two, *hey) = nums
+# print(one)
+# print(two)
+# print(hey)
+
+# print(anotherTuple.count(6))
