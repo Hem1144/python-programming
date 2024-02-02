e878eb37ba32f50e9b8fad47ba0616b84bce2fc9
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-02 15:32:03 +0545

    feat: Added the logic to iterate the loops

---
+++ basic/loops.py
@@ -0,0 +1,43 @@
+# While loop with break
+
+# value = 1
+# while value < 10:
+#     print(value)
+#     if value == 5:
+#         break
+#     value += 1
+
+#! continue
+# value = 1
+# while value < 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to ", str(value))
+
+#! for loop
+names = ["Hemlal", "Prasad", "Dulal"]
+# for x in names:
+#     print(x)
+
+# for y in "hemlal":
+#     print(y)
+
+# for x in names:
+#     if x == "Prasad":
+#         break
+#     print(x)
+
+# for x in names:
+#     if x == "Prasad":
+#         continue
+#     print(x)
+
+#! range()
+# for x in range(4):
+#     print(x)
+
+for x in range(5, 101, 5):
+    print(x)
