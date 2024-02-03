3ecfe25b494c99e913477562ce9a4a702339ad38
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-03 15:42:46 +0545

    feat: Implemented the code that illusrate the scope in python

--- basic/scope.py
+++ basic/scope.py
@@ -1,11 +1,42 @@
-name = "Hemlal"
+# name = "Hemlal"
 
 
-def greeting():
-    color = "blue"
-    print(color)
-    print(name)
+# def greeting(name):
+#     color = "blue"
+#     print(color)
+#     print(name)
 
 
-greeting()
+# greeting("Hemlal Dulal")
 # print(color)  # * can not access due to local scope
+
+
+# def another():
+#     greeting("Dulal")
+
+
+# another()
+
+
+#! function insdie another function
+
+name = "Hemlal"
+count = 1
+
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("Dulal")
+
+
+another()
