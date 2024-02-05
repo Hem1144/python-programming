8c53a6f62253a2c98433a419414ea92d3ec302c5
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-05 22:24:12 +0545

    feat: Wrote the lambda functional logic in python

---
+++ lambdaAndHOF/lambda.py
@@ -0,0 +1,19 @@
+#! first method
+# def square(num):
+#     return num * num
+
+
+# print(square(2))
+
+#! second method using lambda
+square = lambda num: num * num
+print(square(3))
+
+#! using lambda
+# addTwo = lambda num: num + 2
+
+# print(addTwo(12))
+
+
+sum = lambda a, b: a + b
+print(sum(4, 2))
