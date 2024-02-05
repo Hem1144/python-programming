dfeeec9746643ed4e317eab73b24be771ed7ad14
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-05 15:30:33 +0545

    feat: Added import of kansas module and call its functions

--- modules/modules.py
+++ modules/modules.py
@@ -2,7 +2,15 @@ import math
 import sys
 import random as rdm
 from enum import Enum
+import kansas
 
 print(math.pi)
-for item in dir(rdm):
-    print(item)
+
+# for item in dir(rdm):
+#     print(item)
+
+print(kansas.capital)
+kansas.randomfunfact()
+
+print(__name__)
+print(kansas.__name__)  # * it gives the file name
