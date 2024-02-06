6fe43031e3fc76a01a320b89ae9e192442e18153
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-06 16:40:59 +0545

    feat: Created simple class and also invoke Object method

---
+++ classes/classes.py
@@ -0,0 +1,21 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def moves(self):
+        print("Moves along...")
+
+    def get_make_model(self):
+        print(f"I'am a {self.make} {self.model}.")
+
+
+my_car = Vehicle("Tesla", "Model 3")
+# print(my_car.make)
+# print(my_car.model)
+my_car.get_make_model()
+my_car.moves()
+
+your_car = Vehicle("Audi", "v8")
+your_car.get_make_model()
+your_car.moves()
