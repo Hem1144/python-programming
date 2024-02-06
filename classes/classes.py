2f5aa648554634861660723dfb783b9b35098dd5
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-06 17:07:30 +0545

    Feat: Added inheritance to the Vehicle class with three new subclasses: Airplane, Truck, and GolfCart. Each subclass has its own implementation of the moves() method

--- classes/classes.py
+++ classes/classes.py
@@ -19,3 +19,34 @@ my_car.moves()
 your_car = Vehicle("Audi", "v8")
 your_car.get_make_model()
 your_car.moves()
+
+
+#! Inheritance
+class Airplane(Vehicle):
+    def __init__(self, make, model, faa_id):
+        super().__init__(make, model)
+        self.faa_id = faa_id
+
+    def moves(self):
+        print("file along...")
+
+
+class Truck(Vehicle):
+    def moves(self):
+        print("Truck along...")
+
+
+class GolfCart(Vehicle):
+    pass
+
+
+cessna = Airplane("Cessna", "Random", "K532")
+mack = Truck("Mack", "Ruva")
+golf = GolfCart("Wagon", "G200")
+
+cessna.get_make_model()
+cessna.moves()
+mack.get_make_model()
+mack.moves()
+golf.get_make_model()
+golf.moves()
