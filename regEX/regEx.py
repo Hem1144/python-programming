ce90073c655a4511b0c74df97f9b1ee61cf6b72d
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-29 22:44:30 +0545

    feat: regEx to validate email address

--- regEX/regEx.py
+++ regEX/regEx.py
@@ -3,8 +3,14 @@ import re
 str = "1, Hemlal Dulal,+977-9898989889, dulal@gmail.com,+977-9860606162,"
 
 # matched = re.search("[0-9]{10,10}", str)
-matched = re.findall(
-    "\+[0-9]{3,3}-[0-9]{10,10}", str
-)  #! number of matched pattern in a list
 
+#! number of matched pattern in a list
+# matched = re.findall(
+#     "\+[0-9]{3,3}-[0-9]{10,10}", str
+# )
+
+# print(matched)
+
+#! Email validation
+matched = re.search("[0-9a-z]{1,50}@[0-9a-z]{1,50}\.[a-z]{2,4}", str)
 print(matched)
