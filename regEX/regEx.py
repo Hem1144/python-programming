7afaf1bfffe9f6db7d780626d3cf4b8c2494001e
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-02-29 22:38:56 +0545

    feat: return number of matched pattern in a list

--- regEX/regEx.py
+++ regEX/regEx.py
@@ -1,6 +1,10 @@
 import re
 
-str = "1, Hemlal Dulal, dulal@gmail.com,9860606162"
+str = "1, Hemlal Dulal,+977-9898989889, dulal@gmail.com,+977-9860606162,"
 
-matched = re.search("[0-9]{10,10}", str)
+# matched = re.search("[0-9]{10,10}", str)
+matched = re.findall(
+    "\+[0-9]{3,3}-[0-9]{10,10}", str
+)  #! number of matched pattern in a list
+
 print(matched)
