ba5cc6a28d82a6ce8a2476f8900a089b468891c6
Hemlal Dulal <hemlaldulal.986@gmail.com>
2024-03-02 14:35:01 +0545

    feat: Applied decorators to validate email

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,9 +1,36 @@
-def greet():
-    def welcome():
-        print("Greeting")
+# def greet():
+#     def welcome():
+#         print("Greeting")
 
-    return welcome
+#     return welcome
 
 
-func = greet()
-func()
+# func = greet()
+# func()
+
+import re
+
+
+def isEmail(signup):
+    def inner(*args):
+        print("Inside inner function")
+        matched = re.search("[0-9a-z]{1,50}@[0-9a-z]{1,50}\.[a-z]{2,4}", args[1])
+        if matched:
+            print("Calling signup function")
+            signup(args[0], args[1], args[2])
+        else:
+            print("Please enter a valid email address.")
+
+        # If email is valid then call signup function
+        # signup("dulal", "dulal@gmail.com", "123123")
+        # else print some error message
+
+    return inner
+
+
+@isEmail
+def signup(name, email, password):
+    print("Signup successfully")
+
+
+signup("dulal", "dulal@gmail.com", "123123")
